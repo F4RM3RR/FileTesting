896e57107290394a823ed0c80992c9ca3e16114b
F4RM3RR <162732118+F4RM3RR@users.noreply.github.com>
2024-03-08 10:47:13 -0600

    Create pythontest.py

---
+++ pythontest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+import hashlib
+import io
+import json
+import os
+import subprocess
+from string import Template
+import tarfile
+
+def main():
+    print("Hello World);
